39b61a2c3d470b726051fd45569919a51dcec771
d4rksh3ll-PT <d4rksh3ll@gmail.com>
2016-09-18 00:55:13 +0000

    .

--- SEaT/Properties/AssemblyInfo.cs
+++ SEaT/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SEaT")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Share Enumeration and Test")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("d4rksh3ll")]
 [assembly: AssemblyProduct("SEaT")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
