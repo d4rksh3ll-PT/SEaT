9a02dc323d757d17582963b5e2d30789fa29b843
d4rksh3ll-PT <d4rksh3ll@gmail.com>
2016-09-19 01:10:43 +0000

    ..

--- SEaT/Properties/AssemblyInfo.cs
+++ SEaT/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2016.9.19.0051")]
-[assembly: AssemblyFileVersion("2016.9.19.0051")]
+[assembly: AssemblyVersion("2016.9.19.0110")]
+[assembly: AssemblyFileVersion("2016.9.19.0110")]
